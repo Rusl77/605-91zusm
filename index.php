3093c38e9e6999af8c1c2c74c20135352c6d1e9b
Rusl77 <rusl.dzhan@gmail.com>
2024-04-02 17:59:29 +0500

    Подключение к БД и вывод таблицы

--- index.php
+++ index.php
@@ -1,4 +1,113 @@
 <?php
-phpinfo();
-phpinfo();
-sdasad
+require "dbconnect.php";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+    <style>
+        body{
+            max-width: 1100px;
+        }
+        *{
+            font-family: Arial;
+            margin: 0 auto;
+        }
+        .item{
+
+            background-color: palegreen;
+            margin: 20px;
+            padding: 10px;
+            border-radius: 10px;
+
+
+        }
+        .name{
+            vertical-align: top;
+            width: 20%;
+            max-width: 250px;
+            display: inline-block;
+        }
+        .props{
+            vertical-align: top;
+            width: 57%;
+            display: inline-block;
+        }
+        .price{
+            vertical-align: top;
+            width: 10%;
+            max-width: 150px;
+            display: inline-block;
+            font-weight: bold ;
+            text-align: right;
+            font-size: large;
+            margin-right: 15px;
+        }
+        .image{
+            width: 10%;
+            display: inline-block;
+        }
+        .bin-button{
+            align: right;
+            padding: 10px;
+            margin: 5px;
+            border-radius: 8px;
+            border-width: 1px;
+            border-color: green;
+            width: 120px;
+        }
+
+    </style>
+</head>
+<body>
+<h1>Веб-приложение банковские счета</h1>
+
+<?php
+echo "<h2>Интернет-банк</h2>";
+
+$result = $conn->query("SELECT * FROM bankhold.clients");
+
+while ($row = $result->fetch()){
+    ?>
+    <div class="item">
+
+        <div class="id">
+            <?=$row['id']?>
+        </div>
+        <div class="last">
+            <?=$row['last_name']?>
+        </div>
+        <div class="first">
+            <?=$row['first_name']?>
+        </div>
+        <div class="middle">
+            <?=$row['middle_name']?>
+        </div>
+        <div class="birth">
+            <?=$row['birth_date']?>
+        </div>
+        <div class="passport">
+            <?=$row['passport_details']?>
+        </div>
+        <div class="address">
+            <?=$row['address']?>
+        </div>
+        <div class="contact">
+            <?=$row['contact_phone']?>
+        </div>
+        <button class="bin-button"></i>&nbsp&nbsp Параметры счета</button>
+    </div>
+    <?php
+
+}
+?>
+
+
+</body>
+</html>
